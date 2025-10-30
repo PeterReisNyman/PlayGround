d0e0261840b14c6b94e2ef60a597b08c4ceb271b
Peter Nyman <skovdepeter@gmail.com>
2025-10-30 14:12:24 +0100

    a

--- src/realtor/scrape-and-organize.py
+++ src/realtor/scrape-and-organize.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 
 # Function to decode unicode escapes
 def decode_unicode(text):
